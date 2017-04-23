1e531fc3ea0624cd42723850f8958275556217d3
Lucas Homand <lucas.homand@epita.fr>
2017-04-23 15:39:37 +0200

    cest le bordel dans la segmentation

--- SEG/cutcutcut.c
+++ SEG/cutcutcut.c
@@ -8,12 +8,12 @@
 
 void Cut(SDL_Surface* img, int xory, int where, int from, int to){
 	if(xory){ //y, vertically
-		for(int y = from ; y < to ; ++y){ 
+		for(int y = from ; y < img->h && y < to ; ++y){ 
 			putpixel(img, where, y, SDL_MapRGB(img->format, 255, 0, 0));
 		}
 	}
 	else{ //x, horizontally
-		for(int x = from ; x < to ; ++x){ 
+		for(int x = from ; x < img->w && x < to ; ++x){ 
 			putpixel(img, x, where, SDL_MapRGB(img->format, 255, 0, 0));
 		}
 	}
@@ -22,27 +22,31 @@ void Cut(SDL_Surface* img, int xory, int where, int from, int to){
 void MakingLines(SDL_Surface* img)
 {
 	int x, y = 0;
-	Uint8 r = 0, g = 0, b = 0;
+	Uint8 r, g, b;
 	while(y < img->h){
 		x = 0;
-		while(x < img->w && r==0 && g==0 && b==0){ 
+		SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
+		while(x < img->w && r != 0){ 
 			SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
 			++x;
 		}
 		if(x == img->w)
-			Cut(img, 1, y, 0, x);
+			Cut(img, 0, y, 0, x);
 		++y;
+		//Display(img);
 	}
 	x = 0;
 	while(x < img->w){
 		y = 0;
-		while(y < img->h && g==0 && b==0){ 
+		SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
+		while(y < img->h && r != 0){ 
 			SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
 			++y;
 		}
 		if(y == img->h)
-			Cut(img, 0, x, 0, y);
+			Cut(img, 1, x, 0, y);
 		++x;
+		//Display(img);
 	}
 }
 
@@ -63,7 +67,7 @@ void MakingLines(SDL_Surface* img)
 	return img;
 }*/
 
-void MakingBlocks(SDL_Surface* img)
+/*void MakingBlocks(SDL_Surface* img)
 {
 	SDL_Surface* dark_img = img;
 	int x = 0, y = 0, making_block = 0;
@@ -89,4 +93,4 @@ void MakingBlocks(SDL_Surface* img)
 
 }
 
-
+*/
