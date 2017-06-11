3f023b620ee6df733cfe13bc5e79ea684dba9ea9
Lucas Homand <lucas.homand@epita.fr>
2017-06-11 16:02:50 +0200

    Elles sont pas belles mes matrices ?

--- TDI/intomatrices.h
+++ TDI/intomatrices.h
@@ -4,7 +4,7 @@
 }*/
 
 struct mat{
-	size_t c, l;
+	int c, l;
 	char* data;
 };
 
@@ -19,7 +19,7 @@ void mark(SDL_Surface* img, int x, int _y){
 	}
 }
 
-/*struct mat* __intoMat(SDL_Surface* img, int x, int y){
+int __intoMat(SDL_Surface* img, FILE *f, int x, int y){
 	int _x = x, x_max = x, y_max = y, i = 0;
 	Uint8 r, g, b;
 	SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
@@ -32,22 +32,31 @@ void mark(SDL_Surface* img, int x, int _y){
 		SDL_GetRGB(getpixel(img, x, y_max), img->format, &r, &g, &b);
 		y_max++;
 	}
-	struct mat* mat = malloc(sizeof(struct mat));
+	/*struct mat* mat = malloc(sizeof(struct mat));
 	mat->c = x_max - x, mat->l = y_max - y;
-	mat->data = malloc((mat->c) * (mat->l) * sizeof(char));
-	printf("   cols = %zu, y = %i, ymax = %i\n\n", mat->c, y, y_max);
+	mat->data = malloc((mat->c) * (mat->l) * sizeof(char));*/
+	printf("   cols = %i, y = %i, ymax = %i\n\n", x_max - x, y, y_max);
 	while(y < y_max){
 		x = _x;
 		while(x < x_max){
 			SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
-			mat->data[i] = r == 0 ? '1' : '0';
-			++i;
+			char curr = r == 0 ? '1' : '0';
+			fputc(curr, f);
+			printf("%i ", x);
+			++x, ++i;
 		}
+		printf("\n");
 		++y;
 	}
-	mat->data[i] = '\n', mat->data[i+1] = '\0';
-	return mat;
-}*/
+	fputc('\n', f);
+	//mat->data[i] = '\n', mat->data[i+1] = '\0';
+	return x_max;
+}
+
+void dispCurr(SDL_Surface* img, int x, int y){
+	putpixel(img, x, y, SDL_MapRGB(img->format, 0, 255, 255));
+}
+
 void intoMatrices(SDL_Surface* img)
 {
 	int x, y = 0;
@@ -61,50 +70,30 @@ void intoMatrices(SDL_Surface* img)
 			SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
 			if(r == g){
 				if(g != b){
-					while(x < img->w && r == g)
+					while(x < img->w && r == g){
 						++x;
+						SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
+					}
 				}
 				else{
 					printf("Char found at %i\n", x);
-					//struct mat* mat = __intoMat(img, x, y);
-					int _x = x, _y = y, x_max = x, y_max = y, i = 0;
-					Uint8 r, g, b;
-					SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
-					while(x_max < img->w && r == g){
-						SDL_GetRGB(getpixel(img, x_max, y), img->format, &r, &g, &b);
-						x_max++;
-					}
-					r = g;
-					while(y_max < img->h && r == g){
-						SDL_GetRGB(getpixel(img, x, y_max), img->format, &r, &g, &b);
-						y_max++;
-					}
-					struct mat* mat = malloc(sizeof(struct mat));
-					mat->c = x_max - x, mat->l = y_max - y;
-					mat->data = malloc((mat->c) * (mat->l) * sizeof(char));
-					printf("   cols = %zu, y = %i, ymax = %i\n\n", mat->c, y, y_max);
-					while(y < y_max){
-						x = _x;
-						while(x < x_max){
-							SDL_GetRGB(getpixel(img, x, y), img->format, &r, &g, &b);
-							mat->data[i] = r == 0 ? '1' : '0';
-							++i;
-						}
-						++y;
-					}
-					mat->data[i] = '\n', mat->data[i+1] = '\0';
-					x = _x, y = _y;
+					int _x = __intoMat(img, f, x, y);
 
 					//char* char = resize(mat);
-					fputs(mat->data, f);
+					
+					//fputs(mat->data, f);
 					mark(img, x, y);
-					x += mat->c;
-					free(mat);
+					//x += mat->c;
+					x = _x;
 				}
 			}
-			else
+
+			else{
+				putpixel(img, x, y, SDL_MapRGB(img->format, 0, 255, 255));
 				++x;
+			}
 		}
+		display_image(img);
 		++y;
 	}
 	fclose(f);
