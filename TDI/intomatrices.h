bff0f0b04410b982678aed100370d05000036900
Lucas Homand <lucas.homand@epita.fr>
2017-06-12 19:20:00 +0200

    resize me fait tilt

--- TDI/intomatrices.h
+++ TDI/intomatrices.h
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 struct mat{
 	int c, l;
 	char* data;
@@ -14,17 +16,20 @@ void mark(SDL_Surface* img, int x, int _y){
 	}
 }
 
-void resize(struct mat* mat, FILE* ft){
-	int x = 0, y = 0;
-	char* resized = malloc(101*sizeof(char));
-	for(; x < mat->c ; ++x){
-		for(; y < mat->l ; ++y){
+void resize(struct mat* mat, FILE* ft, char* resized){
+	int x, y = 0;
+	while(y < mat->l){
+		x = 0;
+		while(x < mat->c){
 			resized[(x*10/mat->c) + (y*10/mat->l) * mat->c] = mat->data[x + y *mat->c];
+			++x;
 		}
+		++y;
 	}
-	mat->data = resized;
+	memcpy(mat->data, resized, 101);
 	mat->c = 10, mat->l = 10;
 	mat->data[100] = '\0';
+	printf("[%s]\n", mat->data);
 	fputs(mat->data, ft);
 	fputc('\n', ft);
 }
@@ -42,7 +47,7 @@ int __intoMat(SDL_Surface* img, FILE *f, int x, int y){
 		SDL_GetRGB(getpixel(img, x, y_max), img->format, &r, &g, &b);
 		y_max++;
 	}
-	fprintf(f, "%i\n", x_max - x);
+	fprintf(f, "%i %i\n", x_max - x, y_max - y);
 	while(y < y_max){
 		x = _x;
 		while(x < x_max){
@@ -91,23 +96,32 @@ void intoMatrices(SDL_Surface* img)
 		}
 		++y;
 	}
+	printf("Found %i char.\n", char_found);
 	fclose(f);
 	f = fopen("tmp", "r");
 	FILE* ft = fopen("training","w+");
 	fprintf(ft, "i,o,%i\n", char_found);
 	struct mat* mat = malloc(sizeof(struct mat));
 	mat->l = 0;
-	char* buf = calloc(1023, sizeof(char));
-	fgets(buf, 1023, f);
-	while((int)buf[0] != EOF){
-		mat->c = (int)buf[0] - 48;
-		if((int)buf[1] != '\n')
-			mat->c = mat->c * 10 + (int)buf[1] - 48;
-		fgets(buf, 1023, f);
-		printf("%s\n, %i", buf, mat->c);
-		for(; buf[mat->c * mat->l + 1] != '\n' ; ++mat->l);
-		mat->data = buf;
-		resize(mat, ft); //print resized 10(?)*10matrix into training.
-		fgets(buf, 1023, f);
+	char* buf = calloc(2048, sizeof(char));
+	char* resized = calloc(101, sizeof(char));
+	fscanf(f, "%i", &mat->c);
+	fgets(buf, 5, f);
+	for(;char_found > 0; char_found--){
+		printf("Char remaining : %i.\n", 1 + char_found);
+		int k = (int)buf[1] == '\0' ? 1 : 2;
+		//int l = k + (int)buf[k + 2] == '\n' ? 2 : 3;
+		mat->l = strtoul(buf, (char**)(buf + k) , 10);
+		//mat->l = strtoul(buf + k + 1, (char**)(buf + l), 10); 
+		fgets(buf, 2048, f);
+		printf("%i %i \n", mat->c, mat->l);
+		//for(; buf[mat->c * mat->l + 1] != '\n' ; ++mat->l);
+		memcpy(mat->data, buf, mat->c * mat->l + 1);
+		resize(mat, ft, resized); //print resized 10(?)*10matrix into training.
+		fscanf(f, "%i", &mat->c);
+		fgets(buf, 5, f);
 	}
+	free(mat);
+	fclose(f);
+	fclose(ft);
 }
